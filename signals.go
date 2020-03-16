ce501a678c3f25391327151de81ac08bb831eb40
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-03-16 15:09:02 +1000

    Добавлена логика для новой тестовой формы. Удалены старые сигналы

--- signals.go
+++ signals.go
@@ -11,24 +11,8 @@ func SignalCloseWindow(win *gtk.Window) error {
 		gtk.MainQuit()
 	})
 	if err != nil {
-		return fmt.Errorf("Инициализация сигнала закрытия окна: %s\n", err)
+		return fmt.Errorf("Инициализация сигнала закрытия окна: %s", err)
 	}
 	return nil
 }
 
-func SignalPushButton(b *gtk.Builder, buttonID string) error {
-	// Инициализация сигнала нажатия кнопки
-	obj, err := b.GetObject(buttonID)
-	if err != nil {
-		return fmt.Errorf("Инициализация нажатия кнопки: %s\n", err)
-	}
-	button := obj.(*gtk.Button)
-
-	_, err = button.Connect("clicked", func() {
-		fmt.Printf("Получена кнопка\n")
-	})
-	if err != nil {
-		return fmt.Errorf("Получение селектора кнопки: %s\n", err)
-	}
-	return nil
-}
--- variables.go
+++ variables.go
@@ -1,32 +1,118 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gotk3/gotk3/gtk"
+	"io/ioutil"
+	"os"
+)
 
 // Количество всех искомых элементов в окне
 const (
-	VarCount = 10
-	EntryCount = 10
-	ButtonCount = 10
-	LabelCount = 10
-
+	VarCount    = 4
+	EntryCount  = 1
+	ButtonCount = 2
+	LabelCount  = 2
 )
 
-
-// Словарь всех полученных элементов страницы, где ключом является ID елемента.
-var WindowElements = make(ElemMap, VarCount)
-
 func GenerateID() {
-	var  EntryList, ButtonList, LabelList []string
-	for i:=1; i<=EntryCount; i++{
+	var EntryList, ButtonList, LabelList []string
+	for i := 1; i <= EntryCount; i++ {
 		EntryList = append(EntryList, fmt.Sprintf("entry_%d", i))
 	}
-	for i:=1; i<=ButtonCount; i++{
+	for i := 1; i <= ButtonCount; i++ {
 		EntryList = append(ButtonList, fmt.Sprintf("button_%d", i))
 	}
-	for i:=1; i<=LabelCount; i++{
+	for i := 1; i <= LabelCount; i++ {
 		EntryList = append(LabelList, fmt.Sprintf("label_%d", i))
 	}
 
 }
 
+func InitGlobalList(builder *gtk.Builder) {
+	// Словарь всех полученных элементов страницы, где ключом является ID елемента.
+	//var WindowElements = make(ElemMap, VarCount)
+	var (
+		createButton = Element{
+			Name:     "create",
+			Id:       "create",
+			ElemType: "button",
+			Signal:   "clicked",
+			Builder:  builder,
+		}
 
+		resultLabel = Element{
+			Name:     "label",
+			Id:       "label",
+			ElemType: "label",
+			Builder:  builder,
+		}
+
+		inputField = Element{
+			Name:     "input",
+			Id:       "input",
+			ElemType: "input",
+			Builder:  builder,
+		}
+		textArea = Element{
+			Name:     "text_area",
+			Id:       "text_area",
+			ElemType: "area",
+			Builder:  builder,
+		}
+		textBuffer = Element{
+			Name:     "textbuffer1",
+			Id:       "textbuffer1",
+			ElemType: "textBuffer",
+			Builder:  builder,
+		}
+
+		FList      []os.FileInfo
+		resultText string
+
+	)
+
+	btnCreate, err := createButton.GetButton()
+	MyLogger.CheckErr("", err)
+
+	Label, err := resultLabel.GetLabel()
+	MyLogger.CheckErr("", err)
+
+	input, err := inputField.GetEntry()
+	MyLogger.CheckErr("", err)
+
+	dirList, err := textArea.GetTextArea()
+	MyLogger.CheckErr("", err)
+
+	buf, err := textBuffer.GetTextBuffer()
+	MyLogger.CheckErr("", err)
+
+	_, err = btnCreate.Connect(createButton.Signal, func() {
+		inputPath, err := input.GetText()
+		MyLogger.CheckErr("Get text from input field", err)
+
+		fmt.Println("Get text: ", inputPath)
+
+		if err == nil {
+			Label.SetText(inputPath)
+			input.SetText("")
+			FList = GetDirList(inputPath)
+			resultText = ""
+			for _, f := range FList {
+				resultText = resultText + f.Name() + "\n"
+			}
+		}
+
+		buf.SetText(resultText)
+
+		dirList.SetBuffer(buf)
+		fmt.Println("Done")
+	})
+
+}
+
+func GetDirList(path string) []os.FileInfo {
+	result, err := ioutil.ReadDir(path)
+	MyLogger.CheckErr("Reading directory: "+path, err)
+	return result
+}
